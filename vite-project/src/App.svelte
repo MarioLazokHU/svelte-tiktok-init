095008623119dc9424fa07e4edbc11f2ba844e42
mario <lazokmarios7@gmail.com>
2023-08-31 11:02:51 +0200

    init

---
+++ vite-project/src/App.svelte
@@ -0,0 +1,87 @@
+<script>
+  import svelteLogo from "./assets/svelte.svg";
+  import viteLogo from "/vite.svg";
+</script>
+
+<main>
+  <div>
+    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+      <img src={viteLogo} class="logo" alt="Vite Logo" />
+    </a>
+    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
+      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    </a>
+  </div>
+  <h1>Vite + Svelte</h1>
+
+  <blockquote
+    class="tiktok-embed"
+    cite="https://www.tiktok.com/@dainezdainez/video/7266619578166414597"
+    data-video-id="7266619578166414597"
+    style="max-width: 605px;min-width: 325px;"
+  >
+    <section>
+      <a
+        target="_blank"
+        title="@dainezdainez"
+        href="https://www.tiktok.com/@dainezdainez?refer=embed">@dainezdainez</a
+      >
+      dolly chai wala
+      <a
+        title="comidaderua"
+        target="_blank"
+        href="https://www.tiktok.com/tag/comidaderua?refer=embed"
+        >#comidaderua</a
+      >
+      <a
+        title="comidaderuaindiana"
+        target="_blank"
+        href="https://www.tiktok.com/tag/comidaderuaindiana?refer=embed"
+        >#comidaderuaindiana</a
+      >
+      <a
+        title="india"
+        target="_blank"
+        href="https://www.tiktok.com/tag/india?refer=embed">#india</a
+      >
+      <a
+        title="indian"
+        target="_blank"
+        href="https://www.tiktok.com/tag/indian?refer=embed">#indian</a
+      >
+      <a
+        title="tiktokfood"
+        target="_blank"
+        href="https://www.tiktok.com/tag/tiktokfood?refer=embed">#tiktokfood</a
+      >
+      <a
+        title="streetfood"
+        target="_blank"
+        href="https://www.tiktok.com/tag/streetfood?refer=embed">#streetfood</a
+      >
+      <a
+        target="_blank"
+        title="♬ som original - bruno dainez"
+        href="https://www.tiktok.com/music/som-original-7266619669037615877?refer=embed"
+        >♬ som original - bruno dainez</a
+      >
+    </section>
+  </blockquote>
+  <script async src="https://www.tiktok.com/embed.js"></script>
+
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+</style>
